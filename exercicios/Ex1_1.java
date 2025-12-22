64020dbe6f9b96ebe3f839ca03f845401c2e3731
LucasRomero03 <romerocorrea@outlook.com>
2025-12-22 20:26:36 -0300

    exercicio 1

---
+++ exercicios/Ex1_1.java
@@ -0,0 +1,16 @@
+import java.time.OffsetDateTime;
+import java.util.Scanner;
+
+public class Ex1_1{
+
+public static void main(String[] args) {
+    var today = OffsetDateTime.now().getYear();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite Seu Nome: ");
+    var nome = sc.nextLine();
+    System.out.println("Digite o ano de seu nascimento: ");
+    int ano = sc.nextInt();
+    System.out.printf("Sua idade é %d:",today - ano  );
+}
+
+}
